091f6c66d463481e1cfea7f58f838be155c34104
ManuelGuzman0321 <braxfild@gmail.com>
2024-05-23 00:15:14 -0600

    por fin

---
+++ src/tarea_sockets_servidor_manuelguzman/Tarea_Sockets_Servidor_ManuelGuzman.java
@@ -0,0 +1,15 @@
+
+package tarea_sockets_servidor_manuelguzman;
+
+
+public class Tarea_Sockets_Servidor_ManuelGuzman {
+
+    
+    public static void main(String[] args) {
+        
+        Interfacez a = new Interfacez();
+        a.setVisible(true);
+        a.setLocationRelativeTo(null);
+    }
+    
+}
